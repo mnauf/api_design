977f00c2bda9fa91be5bf2e3ec489795ef63f0a7
Naufil <m.naufil1@gmail.com>
2020-05-04 13:52:04 +0500

    removed unnecessary commented code

--- src/resources/men/men.controllers.js
+++ src/resources/men/men.controllers.js
@@ -1,15 +1,3 @@
-// exports.apiGET = function(request,response){
-//     response.send("I am men get")
-// }
-// exports.apiPOST = function(request,response){
-//     response.send("I am men post")
-// }
-// exports.apiPUT = function(request,response){
-//     response.send("I am men put")
-// }
-// exports.apiDELETE = function(request,response){
-//     response.send("I am men delete")
-// }
 const crud = require("../../utils/crud");
 const men = require("./men.model");
 
--- src/resources/women/women.controllers.js
+++ src/resources/women/women.controllers.js
@@ -1,15 +1,3 @@
-// exports.apiGET = (request,response)=>{
-//     response.send("I am women get")
-// }
-// exports.apiPOST = (request,response)=>{
-//     response.send("I am women post")
-// }
-// exports.apiPUT = (request,response)=>{
-//     response.send("I am women put")
-// }
-// exports.apiDELETE = (request,response)=>{
-//     response.send("I am women delete")
-// }
 const crud = require("../../utils/crud");
 const women = require("./women.model");
 
